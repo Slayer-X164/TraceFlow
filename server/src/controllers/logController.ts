6a4f6918fd16108ed9e1aafe6a52f83a3d84c842
Slayer <siddheshghag150@gmail.com>
2026-04-02 22:26:28 +0530

    feature: sending real event log (js_error or resource_error) to /log api endpoint

--- server/src/controllers/logController.ts
+++ server/src/controllers/logController.ts
@@ -1,10 +1,13 @@
 import type { Request, Response } from "express";
 
-export const receivelog = async (req:Request,res:Response) => {
-
-  console.log("Received Logs ",req.body)
-
+export const receivelog = async (req: Request, res: Response) => {
+  console.log({
+    eventLog: req.body,
+    publicApiKey: req.headers["x-api-key"]
+  })
   res.status(200).json({
-    success:true
+    success: true,
+    eventLog: req.body,
+    publicApiKey: req.headers["x-api-key"]
   })
 }
