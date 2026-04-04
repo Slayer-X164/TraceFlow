163bc47b683bf67941e50b898dbac64da9dd502e
Slayer <siddheshghag150@gmail.com>
2026-04-04 22:30:23 +0530

    feature: implement inmemory deduplication of events (server receives only 1 same event within 5sec time period)

--- sdk/sdk.js
+++ sdk/sdk.js
@@ -1,27 +1,51 @@
 //this iss a iif immediately invoked function
 (function () {
   window.TraceFlow = { //attaching TraceFlow object to window i.e global object
-    publicApiKey: "justatestkey786fd",
-    init: function () {
+    publicApiKey: "justatestkey786",
+    initialized: false,
+    errorMap: new Map(),
 
+    init: function () {
+      if (this.initialized) return; //if the sdk is already initialized then user cant call it again
+      this.initialized = true;
       const publicApiKey = this.publicApiKey
 
+      const shouldSendEvent = (eventLog) => {
+        let fingerprint = `${eventLog.eventType}-${eventLog.message || "no-msg"}-${eventLog.filename || "no-file"}-${eventLog.lineno || 0}-${eventLog.url}`
+        let currTime = Date.now()
+
+        let lastSeenTime = this.errorMap.get(fingerprint)
+        // checking if same event happenend within 10sec
+        if (lastSeenTime && currTime - lastSeenTime < 5000) {
+          return false
+        }
+        // saving the fingerprint and currTime in map if not already saved
+        this.errorMap.set(fingerprint, currTime)
+
+        if (this.errorMap.size > 500) {
+          this.errorMap.clear()
+        }
+
+        return true
+      }
+      // for normal events / resource events
       window.addEventListener("error", (event) => {
         const { message, lineno, colno, error, filename } = event
-        const resource = event.target
-        const resourceUrl = resource?.src || resource?.href || null
 
         let eventLog = {
           eventType: error ? "js_error" : "resource_error",
           message: error ? message : null,
-          filename: filename || resourceUrl,
+          filename: filename || "unknown-resource",
           lineno: error ? lineno : null,
           colno: error ? colno : null,
           error: error ? error.stack : null,
           userAgent: window.navigator.userAgent,
-          url:window.location.href,
+          url: window.location.href,
           timestamp: new Date().toISOString()
         }
+
+        if(!shouldSendEvent(eventLog)) return
+
         fetch("http://localhost:3000/api/log", {
           method: "POST",
           headers: {
@@ -33,6 +57,46 @@
           console.error("error while sending event log", err)
         })
       })
+      // for async / promise events
+      window.addEventListener("unhandledrejection", (event) => {
+        const { reason } = event;
+        let message = null, stack = null
+
+        if (reason instanceof Error) {
+          message = reason.message
+          stack = reason.stack
+        } else if (typeof reason === "string") {
+          message = reason
+        } else {
+          message = JSON.stringify(reason)
+        }
+
+        let eventLog = {
+          eventType: "promise_error",
+          message: message,
+          filename: null,
+          lineno: null,
+          colno: null,
+          error: stack,
+          userAgent: window.navigator.userAgent,
+          url: window.location.href,
+          timestamp: new Date().toISOString()
+        }
+
+        if(!shouldSendEvent(eventLog)) return
+
+        fetch("http://localhost:3000/api/log", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "x-api-key": publicApiKey
+          },
+          body: JSON.stringify(eventLog)
+        }).catch(err => {
+          console.error("error while sending async event log", err)
+        })
+      })
+
 
     }
   }
--- server/src/controllers/logController.ts
+++ server/src/controllers/logController.ts
@@ -1,10 +1,7 @@
 import type { Request, Response } from "express";
 
 export const receivelog = async (req: Request, res: Response) => {
-  console.log({
-    eventLog: req.body,
-    publicApiKey: req.headers["x-api-key"]
-  })
+  console.log(req.body)
   res.status(200).json({
     success: true,
     eventLog: req.body,
